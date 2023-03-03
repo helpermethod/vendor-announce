c165c1e1cb4de90db0be8384db181bb71ce37b03
helpermethod <oliver@sdkman.io>
2023-03-03 11:56:51 +0100

    fix: Fix broken test

--- src/main/kotlin/io/sdkman/Application.kt
+++ src/main/kotlin/io/sdkman/Application.kt
@@ -1,15 +1,12 @@
 package io.sdkman
 
-import io.ktor.http.HttpStatusCode
 import io.ktor.http.HttpStatusCode.Companion.Created
-import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationEnvironment
 import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.request.receive
-import io.ktor.server.response.respond
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import io.sdkman.twitter.AccessToken
--- src/test/kotlin/io/sdkman/AnnounceSpec.kt
+++ src/test/kotlin/io/sdkman/AnnounceSpec.kt
@@ -5,9 +5,13 @@ import io.kotest.matchers.shouldBe
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.ContentType.Application
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.HttpStatusCode.Companion.Created
 import io.ktor.http.contentType
 import io.ktor.server.application.call
+import io.ktor.server.config.MapApplicationConfig
+import io.ktor.server.config.mergeWith
 import io.ktor.server.response.respond
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
@@ -17,23 +21,35 @@ class AnnounceSpec : WordSpec({
     "POST /announce/struct" should {
         "return the payload as response" {
             testApplication {
+                /*
+                environment {
+                    config = config.mergeWith(
+                        MapApplicationConfig(
+                            "twitter.apiKey" to "",
+                            "twitter.apiSecretKey" to "",
+                            "twitter.accessToken" to "",
+                            "twitter.accessTokenSecret" to "",
+                        )
+                    )
+                }
+                 */
                 externalServices {
                     hosts("https://api.twitter.com") {
                         routing {
                             post("/2/tweets") {
                                 call.respond(
-                                    HttpStatusCode.Created,
+                                    Created,
                                     """
-                                        {
-                                            "data": {
-                                                "edit_history_tweet_ids": [
-                                                    "1631248723278692357"
-                                                ],
-                                                "id": "1631248723278692357",
-                                                "text": "https://github.com/quarkusio/quarkus/releases/tag/2.7.7.Final quarkus 2.7.7 available on SDKMAN!""
-                                            }
+                                    {
+                                        "data": {
+                                            "edit_history_tweet_ids": [
+                                                "1631248723278692357"
+                                            ],
+                                            "id": "1631248723278692357",
+                                            "text": "https://github.com/quarkusio/quarkus/releases/tag/2.7.7.Final quarkus 2.7.7 available on SDKMAN!""
                                         }
-                                        """.trimIndent()
+                                    }
+                                    """.trimIndent()
                                 )
                             }
                         }
@@ -42,20 +58,19 @@ class AnnounceSpec : WordSpec({
 
                 val response =
                     client.post("/announce/struct") {
-                        contentType(ContentType.Application.Json)
+                        contentType(Application.Json)
                         setBody(
                             """
                             {
                                 "candidate": "quarkus",
                                 "version": "2.7.7",
-                                "hashtag": "",
                                 "url": "https://github.com/quarkusio/quarkus/releases/tag/2.7.7.Final"
                             }
                             """.trimIndent(),
                         )
                     }
 
-                response.status shouldBe  HttpStatusCode.OK
+                response.status shouldBe Created
             }
         }
     }
